a42f5ba4fe7aff0255889ef3e1a52a2f19219ab5
sachinSingh53 <20it095@jssaten.ac.in>
2024-01-08 00:57:46 +0530

    search functionality and bug fixes

--- routes/house.js
+++ routes/house.js
@@ -17,6 +17,7 @@ const mapboxToken = process.env.MAPBOX_TOKEN;
 const geocoder = mbxGeocoding({accessToken:mapboxToken});
 
 
+
 //-----------------index----------------------------------
 router.get('/',catchAsync(async(req,res)=>{
     const houses = await House.find({});
@@ -91,6 +92,11 @@ router.put('/:id',isLoggedIn,isAuthor,catchAsync(async(req,res)=>{
 router.delete('/:id',isLoggedIn,isAuthor,catchAsync(async(req,res)=>{
 
     const {id} = req.params;
+    const house = await House.findById(id);
+    for (let itr of house.images) {
+        await cloudinary.uploader.destroy (itr.filename);
+     //    console.log(filename);
+    }
     await House.findByIdAndDelete(id);
     req.flash('success','Sussessfully Deleted House!');
     res.redirect('/houses',);
@@ -131,6 +137,39 @@ router.delete('/:id/deleteImages',isLoggedIn,isAuthor,catchAsync(async(req,res)=
 
     res.redirect(`/houses/${id}`);
     
-}))
+}));
+
+//------------------------------Finding houses by its location---------------------------
+router.post('/findbylocation',catchAsync(async(req,res)=>{
+
+    const geodata =  await geocoder.forwardGeocode({
+        query:req.body.location,
+        limit:1
+    }).send();
+
+    // console.log( geodata.body.features[0].geometry.coordinates[0]);
+
+   const longitude =  geodata.body.features[0].geometry.coordinates[0];
+   const latitude = geodata.body.features[0].geometry.coordinates[1];
+    
+    console.log(geodata.body.features[0].geometry.coordinates);
+    const houses = await House.aggregate([
+        {
+          $geoNear: {
+            near: { type: "Point", coordinates: [parseFloat(longitude),parseInt(latitude)] },
+            key: "geometry",
+            maxDistance: parseInt(50)*1609, // Max distance in meters (assuming the distance is in miles)
+            distanceField: "dist.calculated",
+            spherical: true
+          }
+        }
+      ]);
+    
+    // console.log(houses);
+    res.render('house/index',{houses});
+    // res.redirect('/houses');
+}));
+
+
 
 module.exports = router;
